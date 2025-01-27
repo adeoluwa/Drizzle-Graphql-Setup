aa2a7d388bfe5be2fbee68431d2fd4a37e139fff
Deolu Joseph <kiishijoseph@gmail.com>
2025-01-27 14:41:34 +0100

    chore: minor clean ups

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,37 +1,22 @@
-// import { drizzle } from 'drizzle-orm/postgres-js';
-// import postgres from 'postgres'; 
-// import * as schema from './schema'; 
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// // Initialize the postgres client using the connection URL
-// const sql = postgres(process.env.DB_URL!, {
-//   ssl: process.env.NODE_ENV === 'production', // Enable SSL for production if required
-// });
-
-
-// if (!sql) {
-//   console.error('Failed to initialize the PostgreSQL client');
-//   process.exit(1); 
-// }
-
-// export const db = drizzle(sql, {
-//   schema,
-// });
-
-
 import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import { Pool } from 'pg';
+import postgres from 'postgres'; 
+import * as schema from './schema'; 
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sql = postgres(process.env.DATABASE_URL!, {
+// Initialize the postgres client using the connection URL
+const sql = postgres(process.env.DB_URL!, {
   ssl: process.env.NODE_ENV === 'production', // Enable SSL for production if required
 });
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+
+if (!sql) {
+  console.error('Failed to initialize the PostgreSQL client');
+  process.exit(1); 
+}
+
+export const db = drizzle(sql, {
+  schema,
 });
 
-export const db = drizzle(sql);
+
